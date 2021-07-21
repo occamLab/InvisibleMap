7877fbcdd59af079206098bcfaf1745faa678fd2
al-li-son <allisonli18710@gmail.com>
2021-07-21 09:50:30 -0400

    Update gitignore to ignore pods

--- Pods/Target Support Files/Pods-InvisibleMap/Pods-InvisibleMap-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_InvisibleMap : NSObject
-@end
-@implementation PodsDummy_Pods_InvisibleMap
-@end
--- Pods/Target Support Files/SwiftGraph/SwiftGraph-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftGraph : NSObject
-@end
-@implementation PodsDummy_SwiftGraph
-@end
