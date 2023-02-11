e400155e00a3281b60f2c856f43e872e7ac4dc0c
joyce061888 <108084198+joyce061888@users.noreply.github.com>
2023-02-11 11:54:54 -0500

    Started Clew 2.0 AppState and AppController

---
+++ Clew 2.0/Clew2App.swift
@@ -0,0 +1,18 @@
+//
+//  Clew2App.swift
+//  Clew 2.0
+//
+//  Created by Joyce Chung & Gabby Blake on 2/11/23.
+//  Copyright © 2023 Occam Lab. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Clew2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
