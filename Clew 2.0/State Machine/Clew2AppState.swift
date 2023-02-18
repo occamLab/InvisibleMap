3e94ae988a911080216fbc9e6b605497bb961e3b
joyce061888 <108084198+joyce061888@users.noreply.github.com>
2023-02-18 16:41:04 -0500

    Added the outline for States, Commands, Events for Clew 2.0

--- Clew 2.0/State Machine/Clew2AppState.swift
+++ Clew 2.0/State Machine/Clew2AppState.swift
@@ -11,118 +11,195 @@ import ARKit
 
 indirect enum Clew2AppState: StateType {
     // Higher level app states
-    case MainScreen
-    case NavigateMap
-    case EditMap
-    case SelectPath(lastState: InvisibleMapAppState)
-    case PreparingToLeaveMap
+    case HomeScreen
+    case FamilyScreen
+    case LocationScreen
+    case POIScreen
+    case CreateMap(CreateMapState)
+    case NavigateMap(NavigateMapState)
     
     // Initial state upon opening the app
-    static let initialState = InvisibleMapAppState.MainScreen
+    static let initialState = Clew2AppState.HomeScreen
     
     // All the effectual inputs from the app which the state can react to
     enum Event {
-        // MainScreen events
-        case MapSelected(mapFileName: String)
-        // SelectPath events
-        case PathSelected(locationType: String, Id: Int)
+        // HomeScreen events
+        case CreateMapRequested
+        case LocateUserRequested
+        case LocationSelected
+        case DomainSelected(mapName: String) // top of name hierarchy (i.e. Food & Drinks)
+        case FamilySelected(mapName: String) // next in name hierarchy (i.e. Restaurants - one option in Food & Drinks)
+        case LocationSelected(mapName: String) // (i.e. Cheescake Factory - a restaurant)
+        case ReviewsSelected(mapName: String) // users want to see the reviews for the POIs in this map
+        case POISelected(mapName: String) // last in name hierarchy (i.e. Restroom in Cheescake Factory)
+        
+        // CreateMap events
+        case DropGeospatialAnchorRequested
+        case DropCloudAnchorRequested
+        case DropDoorAnchorRequested
+        case DropStairAnchorRequested
+        case ViewPOIsRequested
+        case SaveMapRequested(mapName: String)
+        case CancelMapRequested
+        case NewARFrame(cameraFrame: ARFrame) // to update AR screen during map creation
+        case NewTagFound(tag: AprilTags, cameraTransform: simd_float4x4, snapTagsToVertical: Bool) // if we are still using April tags
+        case PlanesUpdated(planes: [ARPlaneAnchor])
+        
         // NavigateMap events
-        case NewARFrame(cameraFrame: ARFrame)
-        case TagFound(tag: AprilTags, cameraTransform: simd_float4x4)
-        case EndpointReached(finalEndpoint: Bool)
-        case EditMapRequested
-        case CancelEditRequested
-        case SaveEditRequested
-        case ViewPathRequested
-        case DismissPathRequested
-        case LeaveMapRequested(mapFileName: String)
-        case ReadyToLeaveMap(mapFileName: String)
-        case PlanPath
-       // case HomeRequested
+        case PreviewDirectionSelected(mapName: String)
+        case LeaveMapRequested(mapName: String) // takes users to POIScreen state
+        case ChangeRouteRequested(mapName: String) // we may need to save the mapName so that we can redirect users to a new POI destination
+        case EndpointReached
+        case RateMapRequested(mapName: String)
+        case HomeScreenRequested
     }
     
     // All the effectful outputs which the state desires to have performed on the app
     enum Command {
-        case LoadMap(mapFileName: String)
-        case StartPath(locationType: String, Id: Int)
-        case UpdatePoseVIO(cameraFrame: ARFrame)
-        case UpdatePoseTag(tag: AprilTags, cameraTransform: simd_float4x4)
-        case GetNewEndpoint
-        case EditMap
-        case FinishedNavigation
-        case PrepareToLeaveMap(mapFileName: String)
-        case LeaveMap(mapFileName: String)
-        case PlanPath
-        case UpdateInstructionText
+        // HomeScreen commands
+        case LoadFamilyScreen(mapName: String)
+        case LoadLocations(mapName: String)
+        case LoadPOIs(mapName: String)
+        
+        // CreateMap commands
+        case LocateAndCategorizeMap // user uses GPS to automatically categorize the map - map still needs to be named
+        case NameMap(mapName: String)
+        case LoadAndCategorizeMap(mapName: String) // user searches for a location that doesn't have a map yet and creates a map for that location - map already named
+        case DropGeospatialAnchor // anchors outside the establishment
+        // * cloud anchors for inside the establishment
+        case DropCloudAnchor
+        case DropDoorAnchor
+        case DropStairAnchor
+        case DropCloudAnchor // cloud anchors are both breadcrumbs and can be named as POIs - need to figure out the time interval at which it should be dropped or if we should make users drop it frequently and name those that they want to
+        case NamePOI
+        case ViewPOIs
+        case SaveMap(mapName: String)
+        case CancelMap
+        
+        // NavigateMap commands
+        case LoadReviews
+        case StartNavigation
+        case LoadPreviewDirections
+        case LeaveMap
+        case ModifyRoute // or StartNavigation? to a new endpoint
+        case LoadRatePopUp
     }
     
     // In response to an event, a state may transition to a new state, and it may emit a command
     mutating func handle(event: Event) -> [Command] {
         print("Last State: \(self), \(event)")
         switch (self, event) {
-            // Note: loads the selected map each time state changes from mainscreen to selectpath (location list view) state; when users go from selectpath to mainscreen state is reset to mainscreen (laststate before selectpath) -> dismisspathrequested (event)
-            case (.MainScreen, .MapSelected(let mapFileName)):
-                self = .SelectPath(lastState: InvisibleMapAppState.MainScreen)
-                return [.LoadMap(mapFileName: mapFileName)]
+        case (.HomeScreen, .CreateMapRequested):
+            self = .NameMapScreen
+            return [.NameMap] // should we send to Firebase after user presses 'Finished'?
             
-            case (.SelectPath, .NewARFrame(let cameraFrame)):
-                return []
+        // user finds a map's POIs without shortcut
+        case (.HomeScreen, .DomainSelected(let mapName)):
+            self = .FamilyScreen
+            return [.LoadFamilyScreen(mapName: mapName)]
+        case (.DomainSelected(let mapName), .FamilySelected(let mapName)):
+            self = .LocationScreen
+            return [.LoadLocations(mapName: mapName)]
+        case (.FamilySelected(let mapName), .LocationSelected(let mapName)):
+            self = .POIScreen
+            return [.LoadPOIs(mapName: mapName)]
+        // user finds a map's POIs through the search bar - takes shortcut
+        case (.HomeScreen, .LocationSelected(let mapName)):
+            self = .POIScreen
+            return [.LoadPOIs(mapName: mapName)]
+        
+        case (.POIScreen, .CreateMapRequested):
+            self = .NameMapScreen
+            return [.NameMap]
+        case (.NameMapScreen, .LocateUserRequested):
+            self = .CreateMapOutside
+            return [.LocateAndCategorizeMap]
+        
+        
             
-            case (.SelectPath, .PathSelected(let locationType, let Id)):
-                self = .NavigateMap
-            return [.StartPath(locationType: locationType, Id: Id)]
-            
-            // Note: dismiss select path view back and set app state back to main screen
-            case (.SelectPath(let lastState), .DismissPathRequested):
-                //self = lastState
-                self = .MainScreen
-                return []
-            
-            // Note: go back to saved location list [SelectPathView] when cancel button is pressed; lastState of SelectPath must be MainScreen in order to reload maps' location lists in SelectPath view
-            case (.NavigateMap, .LeaveMapRequested(let mapFileName)):
-                self = .PreparingToLeaveMap
-            
-                return [.PrepareToLeaveMap(mapFileName: mapFileName)]
-            
-            case (.NavigateMap, .NewARFrame(let cameraFrame)):
-                return [.UpdatePoseVIO(cameraFrame: cameraFrame), .UpdateInstructionText]
-            
-            case (.NavigateMap, .TagFound(let tag, let cameraTransform)):
-                return [.UpdatePoseTag(tag: tag, cameraTransform: cameraTransform)]
-            
-            // Note: As of now this case is when users reach their selected destination
-            case (.NavigateMap, .EndpointReached(let finalEndpoint)):
-               // self = .SelectPath(lastState: InvisibleMapAppState.MainScreen)
-                return [.FinishedNavigation]
-               // return finalEndpoint ? [.GetNewEndpoint] : [.FinishedNavigation]
-            
-            case (.NavigateMap, .EditMapRequested):
-                self = .EditMap
-                return []
-            
-            case (.NavigateMap, .PlanPath):
-                return [.PlanPath]
-            
-            case (.PreparingToLeaveMap, .ReadyToLeaveMap(let mapFileName)):
-                self = .SelectPath(lastState: InvisibleMapAppState.PreparingToLeaveMap)
-                return [.LeaveMap(mapFileName: mapFileName)]
-            
-            
-           /* case (.NavigateMap, .HomeRequested):
-                self = .MainScreen
-                return [] */
-            
-            
-        /*    case (.EditMap, .CancelEditRequested):
-                self = .NavigateMap
-                return []
-         
-            case (.EditMap, .SaveEditRequested):
-                self = .NavigateMap
-                return [.EditMap] */
+
 
             default: break
         }
         return []
+    }
+}
+
+enum CreateMapState: StateType {
+    // Lower level app states nested within CreateMapState
+    case CreateMap
+    case NameMapScreen  // for the purpose of naming hierarchy - to keep maps organized into folders
+    case CreateMapOutside // dropping geospatial anchors
+    case CreateMapInside // dropping cloud anchors
+    case DropDoorAnchor
+    case DropStairAnchor
+    case ViewPOIs // viewing points of interests that the user has dropped
+    
+    // Initial state upon transitioning into the CreateMapState
+    static let initialState = CreateMapState.CreateMap
+    
+    // All the effectual inputs from the app which CreateMapState can react to
+    enum Event {
+        case
+    }
+    
+    // Refers to commands defined in Clew2AppState - adds onto created commands for CreateMapState
+    typealias Command = Clew2AppState.Command
+    
+    // In response to an event, CreateMapState may emit a command
+    mutating func handle(event:Evnet) -> [Command] {
+        switch (self, event) {
+            
+        default: break
+        }
+        return []
+    }
+}
+
+// Translate between events in Clew2AppState and events in CreateMapState
+extension CreateMapState.Event {
+    init?(_ event: Clew2AppState.Event) {
+        switch event {
+            
+        default: return nil
+        }
+    }
+}
+
+enum NavigateMapState: StateType {
+    // Lower level app states nested within NavigateMapState
+    case NavigateMap
+    case ReviewsScreen
+    case NewARView
+    case PreviewDirectionScreen
+    
+    // Initial state upon transitioning into the NavigateMapState
+    static let initialState = NavigateMapState.CreateMap
+    
+    // All the effectual inputs from the app which NavigateMapState can react to
+    enum Event {
+        case
+    }
+    
+    // Refers to commands defined in Clew2AppState - adds onto created commands for NavigateMapState
+    typealias Command = Clew2AppState.Command
+    
+    // In response to an event, CreateMapState may emit a command
+    mutating func handle(event:Evnet) -> [Command] {
+        switch (self, event) {
+            
+        default: break
+        }
+        return []
+    }
+}
+
+// Translate between events in Clew2AppState and events in NavigateMapState
+extension NavigateMapState.Event {
+    init?(_ event: Clew2AppState.Event) {
+        switch event {
+            
+        default: return nil
+        }
     }
 }
